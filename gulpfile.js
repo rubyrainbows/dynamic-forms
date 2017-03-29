55ff97310cf2543aafb0c308c8be537998b2e264
Thomas Muntaner <thomas.muntaner@gmail.com>
2017-03-29 16:53:58 +0200

    Added playground to docs folder

--- gulpfile.js
+++ gulpfile.js
@@ -4,7 +4,7 @@ var watch = require('gulp-watch');
 var uglify = require('gulp-uglify');
 var rename = require('gulp-rename');
 
-gulp.task("default", function () {
+gulp.task('build', function () {
     return gulp.src("src/dynamic_forms.js")
         .pipe(babel())
         .pipe(gulp.dest("dist"))
@@ -13,6 +13,14 @@ gulp.task("default", function () {
         .pipe(gulp.dest("dist"));
 });
 
+gulp.task('build_playground', function () {
+    return gulp.src(["dist/dynamic_forms.min.js", "node_modules/jquery/dist/jquery.min.js"])
+        .pipe(uglify())
+        .pipe(gulp.dest("docs"));
+});
+
+gulp.task("default", ['build','build_playground']);
+
 gulp.task('watch', function () {
-   gulp.watch('src/*.js', ['default']);
+    gulp.watch('src/*.js', ['default']);
 });
